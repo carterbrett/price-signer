4859f762ded918bd10480db568a405a98a0c1238
Brett Carter <brettcarter@Bretts-MacBook-Pro.local>
2022-07-14 17:33:53 -0500

    read pk from .env

--- pages/api/signed_price.js
+++ pages/api/signed_price.js
@@ -4,15 +4,15 @@ import { Signature, PrivateKey, isReady, Field } from 'snarkyjs'
 export default async function handler(req, res) {
   try {
     await isReady;
-
-    const pk = await PrivateKey.random();
+    
+    const pk = PrivateKey.fromBase58(process.env.PK_BASE58);
 
     const response = await fetch(`https://www.bitstamp.net/api/v2/ticker/ethusd/`);
     const data = await response.json();
     const priceInt = parseInt(data.last.replaceAll('.',''));;
 
     const price = new Field(priceInt);
-    const signature = new Signature(price, pk);
+    const signature = Signature.create(pk, [price]);
     const key = pk.toPublicKey();
 
     res.status(200).json({price, signature, key});
